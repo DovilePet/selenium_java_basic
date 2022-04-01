fdb7ab805810534766c1796c12a4bc3f365fadbe
Dovile Petrauskiene <dopetrauskiene@gmail.com>
2022-04-01 10:58:06 +0300

    First commit. Task1

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -14,9 +14,16 @@ public class Sample1Task {
     public void goToHomepage() throws Exception {
 //        TODO:
 //         define driver
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\sauli\\IdeaProjects\\Acc bootcamp\\TABootcampJava2022\\selenium_java_basic\\lib\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
+        driver.get("https://kristinek.github.io/site/index2.html");
 //         get title of page
+        System.out.println("Title is: " + driver.getTitle());
 //         get URL of current page
+        System.out.println("URL:" + driver.getCurrentUrl());
+        Thread.sleep(5000);
 //         close browser
+        driver.quit();
     }
 }
